448ab967881ed6fcbcb0e52d919be90b75cdb110
zhangkai <82190688@qq.com>
2016-09-01 09:04:45 +0800

    call java method from c++

--- IBM/Sample1/Sample1.cpp
+++ IBM/Sample1/Sample1.cpp
@@ -1,15 +1,32 @@
 #include "Sample1.h"
 #include <string.h>
 
+static JNIEnv *jniEnv;
+// 回调函数
+void TestCallback() {
+    jclass cls = jniEnv->FindClass("Sample1");
+    if (cls != 0) {
+      jmethodID mid = jniEnv->GetStaticMethodID(cls, "saySomething", "()V");
+      if (mid != 0) {
+        jniEnv->CallStaticVoidMethod(cls, mid);
+        printf("Call saySomething");
+      }
+    }
+}
+
 JNIEXPORT jint JNICALL Java_Sample1_intMethod(JNIEnv *env, jobject obj, jint num) {
+  printf("Address of env is %p\n", env);
+  jniEnv = env;
   return num * num;
 }
 
 JNIEXPORT jboolean JNICALL Java_Sample1_booleanMethod(JNIEnv *env, jobject obj, jboolean boolean) {
+  printf("Address of jniEnv is %p\n", jniEnv);
   return !boolean;
 }
 
 JNIEXPORT jstring JNICALL Java_Sample1_stringMethod(JNIEnv *env, jobject obj, jstring string) {
+  printf("Address of jniEnv is %p\n", jniEnv);
   const char *str = env->GetStringUTFChars(string, 0);
   char cap[128];
   strcpy(cap, str);
@@ -19,6 +36,7 @@ JNIEXPORT jstring JNICALL Java_Sample1_stringMethod(JNIEnv *env, jobject obj, js
 }
 
 JNIEXPORT jint JNICALL Java_Sample1_intArrayMethod(JNIEnv *env, jobject obj, jintArray array) {
+  printf("Address of jniEnv is %p\n", jniEnv);
   int i, sum = 0;
   jsize len = env->GetArrayLength(array);
   jint *body = env->GetIntArrayElements(array, 0);
@@ -26,5 +44,6 @@ JNIEXPORT jint JNICALL Java_Sample1_intArrayMethod(JNIEnv *env, jobject obj, jin
     sum += body[i];
   }
   env->ReleaseIntArrayElements(array, body, 0);
+  TestCallback();
   return sum;
 }
--- IBM/Sample1/Sample1.java
+++ IBM/Sample1/Sample1.java
@@ -1,12 +1,14 @@
-public class Sample1
-{
+public class Sample1 {
   public native int intMethod(int n);
   public native boolean booleanMethod(boolean bool);
   public native String stringMethod(String text);
   public native int intArrayMethod(int[] intArray);
 
-  public static void main(String[] args)
-  {
+  public static void saySomething() {
+    System.out.println("say something");
+  }
+
+  public static void main(String[] args) {
     System.loadLibrary("Sample1");
     Sample1 sample = new Sample1();
     int square = sample.intMethod(5);
